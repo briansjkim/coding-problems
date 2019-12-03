f0e946c9f52afc539e817c88130ddaaeb96bc3cb
Brian Kim <briaansjkim@gmail.com>
2019-12-03 14:59:04 -0500

    init set up for search

---
+++ algos/search.js
@@ -0,0 +1,9 @@
+// given a sorted array of integers, write a function called search
+// it accepts a value and returns the index where the value passed to the function is located
+// if the value is not found, return -1
+
+// divide and conquer solution
+
+var search = (array, num) => {
+
+}
