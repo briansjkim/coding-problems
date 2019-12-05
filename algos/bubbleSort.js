4a0448021f4484aba10208267e4daac246ed27d7
Brian Kim <briaansjkim@gmail.com>
2019-12-05 14:43:48 -0500

    edited order of functions

--- algos/bubbleSort.js
+++ algos/bubbleSort.js
@@ -6,6 +6,12 @@
   // repeat until the list is sorted
 // return the now sorted array
 
+var swap = (array, idx1, idx2) => {
+  let temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
+}
+
 var bubbleSort = (array) => {
   for (var i = 0; i < array.length; i++) {
     for (var j = i + 1; j < array.length; j++) {
@@ -17,11 +23,6 @@ var bubbleSort = (array) => {
   return array;
 }
 
-var swap = (array, idx1, idx2) => {
-  let temp = array[idx1];
-  array[idx1] = array[idx2];
-  array[idx2] = temp;
-}
 
-console.log(bubbleSort([1, 5, 2, 7, 2, 3]));
-console.log(bubbleSort([10, 11, 1, 3, 7, 2]));
+// console.log(bubbleSort([1, 5, 2, 7, 2, 3]));
+// console.log(bubbleSort([10, 11, 1, 3, 7, 2]));
