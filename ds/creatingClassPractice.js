9355f34086764c125420f4d318e4c17ee6e99968
Brian Kim <briaansjkim@gmail.com>
2019-12-16 16:43:25 -0500

    more practice

--- ds/creatingClassPractice.js
+++ ds/creatingClassPractice.js
@@ -6,5 +6,7 @@ class Student {
 }
 
 var brian = new Student('Brian', 'Kim');
+var hr = new Student('Hack', 'Reactor');
 
-console.log('Hello, my name is ' + brian.firstName + ' ' + brian.lastName);
+// console.log('Hello, my name is ' + brian.firstName + ' ' + brian.lastName);
+// console.log('Hello, my name is ' + hr.firstName + ' ' + hr.lastName);
