2a4e7eee38cc83c1fe4c6f9a8f6cc3bd6df57923
Brian Kim <briaansjkim@gmail.com>
2019-12-16 16:59:40 -0500

    more practice w/ instance methods

--- ds/creatingClassPractice.js
+++ ds/creatingClassPractice.js
@@ -2,10 +2,24 @@ class Student {
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.days = 0;
+    this.scores = [];
   }
 
   fullName() {
     return 'Hello, my name is ' + this.firstName + ' ' + this.lastName;
+  }
+  updateDays() {
+    this.days++;
+    return `Hello, my name is ${this.firstName} ${this.lastName} and I've been here for ${this.days} days`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAvg() {
+    let sum = this.scores.reduce(function(a, b) { return a + b});
+    return sum / this.scores.length;
   }
 }
 
@@ -15,5 +29,15 @@ var hr = new Student('Hack', 'Reactor');
 // console.log('Hello, my name is ' + brian.firstName + ' ' + brian.lastName);
 // console.log('Hello, my name is ' + hr.firstName + ' ' + hr.lastName);
 
-console.log(brian.fullName());
-console.log(hr.fullName());
+// console.log(brian.fullName());
+// console.log(hr.fullName());
+
+// console.log(brian.updateDays());
+// console.log(brian.updateDays());
+// console.log(hr.updateDays());
+
+// brian.addScore(5);
+// brian.addScore(15);
+// brian.addScore(70);
+// brian.addScore(10);
+// console.log(brian.calculateAvg());
