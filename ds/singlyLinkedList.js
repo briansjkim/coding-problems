3176eaa14a7d944c51da7d00080296f1aa8d86f2
Brian Kim <briaansjkim@gmail.com>
2019-12-16 18:03:35 -0500

    implemented pop method

--- ds/singlyLinkedList.js
+++ ds/singlyLinkedList.js
@@ -9,23 +9,48 @@ class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = null;
+    this.length = 0;
   }
 
   push(val) {
-    let newNode = new Node(val);
+    var newNode = new Node(val);
+
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
     } else {
+      this.tail.next = newNode;
       this.tail = newNode;
     }
-    this.head.next = this.tail;
     this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    var current = this.head;
+    var newTail = current;
+    while (current.next !== null) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return this.tail;
   }
 }
 
 // var test = new SinglyLinkedList;
 // test.push(5);
 // test.push(6);
+// test.push(7);
+// test.pop();
 // console.log(test);
