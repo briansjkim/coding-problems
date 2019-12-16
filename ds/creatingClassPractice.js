d930bf032215b53bd3d5ae18779f60aeccdb8e65
Brian Kim <briaansjkim@gmail.com>
2019-12-16 16:47:07 -0500

    practice w/ instance methods

--- ds/creatingClassPractice.js
+++ ds/creatingClassPractice.js
@@ -3,6 +3,10 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+
+  fullName() {
+    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName;
+  }
 }
 
 var brian = new Student('Brian', 'Kim');
@@ -10,3 +14,6 @@ var hr = new Student('Hack', 'Reactor');
 
 // console.log('Hello, my name is ' + brian.firstName + ' ' + brian.lastName);
 // console.log('Hello, my name is ' + hr.firstName + ' ' + hr.lastName);
+
+console.log(brian.fullName());
+console.log(hr.fullName());
