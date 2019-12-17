058cb902afbc90ab936291a258127dc076505dd8
Brian Kim <briaansjkim@gmail.com>
2019-12-17 13:25:40 -0500

    implemented push method

--- ds/stack.js
+++ ds/stack.js
@@ -11,5 +11,19 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var oldFirst = this.first;
+      this.first = newNode;
+      newNode.next = oldFirst;
+    }
+    this.size++;
+    return this.size;
+  }
 }
 
