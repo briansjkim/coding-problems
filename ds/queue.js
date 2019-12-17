d80d63cd87dcb177a026522d818a585c04f7be47
Brian Kim <briaansjkim@gmail.com>
2019-12-17 14:10:05 -0500

    implemented enqueue method

--- ds/queue.js
+++ ds/queue.js
@@ -11,5 +11,18 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this;
+  }
 }
 
