6cb0965c06f1a0d1ce8bd07836b6449134cf8529
Brian Kim <briaansjkim@gmail.com>
2019-12-19 15:59:01 -0500

    edited swap helper

--- ds/minBinaryHeap.js
+++ ds/minBinaryHeap.js
@@ -5,9 +5,10 @@ class MinBinaryHeap {
 
   swap(val) {
     var idx = this.values.length - 1;
+    // node is going to stay the same value
+    let node = this.values[idx];
 
     while (idx > 0) {
-      let node = this.values[idx];
       let parentIdx = Math.floor( (idx - 1/ 2));
       let parent = this.values[parentIdx];
 
