f37d908886da635475f6f7ee8889b6dd22910bed
Brian Kim <briaansjkim@gmail.com>
2019-12-19 11:38:27 -0500

    init set up for DFSPreOrder

---
+++ ds/DepthFirstPreOrder.js
@@ -0,0 +1,80 @@
+// starts at the root node
+// traverses down the whole LEFT side FIRST
+// then traverses down the whole RIGHT side after
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.val = null;
+  }
+
+  insert(val) {
+    var newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (newNode.val === current.val) {
+          return undefined;
+        }
+        if (newNode.val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (newNode.val > current.val) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+    return this;
+  }
+
+  search(val) {
+    if (!this.root) {
+      return false;
+    }
+    if (this.root.val === val) {
+      return true;
+    } else {
+      var current = this.root;
+      while (current) {
+        if (val < current.val) {
+          if (!current.left) {
+            return false;
+          } else if (current.left.val === val) {
+            return true;
+          } else {
+            current = current.left;
+          }
+        } else {
+          if (!current.right) {
+            return false;
+          } else if (current.right.val === val) {
+            return true;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  DFSPreOrder() {
+  }
+}
