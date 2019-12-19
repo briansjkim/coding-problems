38db7250723d59559d816fa544e1f3ba688e29aa
Brian Kim <briaansjkim@gmail.com>
2019-12-19 11:23:07 -0500

    implemented BFS

--- ds/breadthFirstSearch.js
+++ ds/breadthFirstSearch.js
@@ -72,6 +72,17 @@ class BinarySearchTree {
   }
 
   BFS() {
+    var result = [];
+    var queue = [];
+    var node = this.root;
+    queue.push(node);
 
+    while (queue.length) {
+      node = queue.shift();
+      result.push(node);
+      if (node.left) { queue.push(node.left) };
+      if (node.right) { queue.push(node.right) };
+    }
+    return result;
   }
 }
