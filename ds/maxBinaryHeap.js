514973b1a97b5419c4acce3d293bd7e6b0506b48
Brian Kim <briaansjkim@gmail.com>
2019-12-19 15:21:57 -0500

    implemented insert method with swap helper

--- ds/maxBinaryHeap.js
+++ ds/maxBinaryHeap.js
@@ -2,4 +2,27 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  swap(val) {
+    var idx = this.values.length - 1;
+
+    // stop before idx gets to 0 because the parentIdx is going to be -1
+    while (idx > 0) {
+      let node = this.values[idx];
+      let parentIdx = Math.floor( (idx - 1) / 2);
+      let parent = this.values[parentIdx];
+
+      if (node <= parent) { break; }
+
+      this.values[parentIdx] = node;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  insert(val) {
+    this.values.push(val);
+    this.swap(val);
+    return this;
+  }
 }
