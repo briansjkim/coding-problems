8a6bd4f813dcc51dca7a265964f1fef9f79e75f3
Brian Kim <briaansjkim@gmail.com>
2019-12-18 21:58:28 -0500

    implemented insert method

--- ds/binarySearchTree.js
+++ ds/binarySearchTree.js
@@ -8,6 +8,36 @@ class Node {
 
 class BinarySearchTree {
   constructor() {
-    this.root = null;
+    this.val = null;
+  }
+
+  insert(val) {
+    var newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (newNode.val === current.val) {
+          return undefined;
+        }
+        if (newNode.val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (newNode.val > current.val) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+    return this;
   }
 }
