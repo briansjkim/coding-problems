5138309a5eae2531c20dfe973c363b107fa41309
Brian Kim <briaansjkim@gmail.com>
2019-12-19 15:48:52 -0500

    implmeneted insert method w/ swap helper

--- ds/minBinaryHeap.js
+++ ds/minBinaryHeap.js
@@ -2,4 +2,26 @@ class MinBinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  swap(val) {
+    var idx = this.values.length - 1;
+
+    while (idx > 0) {
+      let node = this.values[idx];
+      let parentIdx = Math.floor( (idx - 1/ 2));
+      let parent = this.values[parentIdx];
+
+      if (node >= parent) { break; }
+
+      this.values[parentIdx] = node;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  insert(val) {
+    this.values.push(val);
+    this.swap(val);
+    return this;
+  }
 }
