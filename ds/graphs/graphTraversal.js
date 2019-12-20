126a66454ef73bf29c434fd11b984908dd11ef36
Brian Kim <briaansjkim@gmail.com>
2019-12-20 16:33:07 -0500

    init set up for graph traversal methods

---
+++ ds/graphs/graphTraversal.js
@@ -0,0 +1,43 @@
+// an undirected graph with an adjacency list
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1].includes(v2)) {
+      this.adjacencyList[v1].push(v2);
+    }
+    if (!this.adjacencyList[v2].includes(v1)) {
+      this.adjacencyList[v2].push(v1);
+    }
+    return this;
+  }
+
+  removeEdge(v1, v2) {
+    if (this.adjacencyList[v1].includes(v2)) {
+      this.adjacencyList[v1] = this.adjacencyList[v1].filter( v => v !== v2);
+    }
+    if (this.adjacencyList[v2].includes(v1)) {
+      this.adjacencyList[v2] = this.adjacencyList[v2].filter( v => v !== v1);
+    }
+    return this;
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      var adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(adjacentVertex, vertex);
+    }
+    delete this.adjacentList[vertex];
+    return this;
+  }
+}
+
