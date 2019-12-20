fd817cdd8e8271f2af025301d331a7a8bb834233
Brian Kim <briaansjkim@gmail.com>
2019-12-20 15:04:33 -0500

    implement addVertex method

--- ds/graphs/graph.js
+++ ds/graphs/graph.js
@@ -4,5 +4,11 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 }
 
