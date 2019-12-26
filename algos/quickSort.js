4e1431f21d143ce3314dc435fc81e00476c6170d
Brian Kim <briaansjkim@gmail.com>
2019-12-26 16:57:13 -0500

    implemented quickSort

--- algos/quickSort.js
+++ algos/quickSort.js
@@ -1,5 +1,23 @@
 // pivot/partition helper
 
-var pivot = (arr, start = 0, end = arr.length + 1) {
+var pivot = (arr, start = 0, end = arr.length + 1) => {
+  function swap(arr, idx1, idx2) {
+    var temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+  }
 
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, i, swapIdx);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
 }
+
+
