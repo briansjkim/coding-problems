3ee4bb4ffa45e40b1e2612595e8dda15bed46e18
Brian Kim <briaansjkim@gmail.com>
2020-02-27 11:33:40 -0500

    init set up for priority queue

--- ds/binaryHeaps/priorityQueue.js
+++ ds/binaryHeaps/priorityQueue.js
@@ -1,78 +1,10 @@
-class Node {
-  constructor(val, priority) {
-    this.val = val;
-    this.priority = priority;
-    // if priorities are identical and you want to dequeue the node that was inserted first
-    this.insertTime = Date.now();
-  }
+function Node(val, priority) {
+  this.val = val;
+  this.priority = priority;
 }
 
 class PriorityQueue {
   constructor() {
     this.values = [];
-  }
-
-  enqueue(val, priority) {
-    let newNode = new Node(val, priority);
-    this.values.push(newNode);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let idx = this.values.length - 1;
-    var element = this.values[idx];
-
-    while (idx > 0) {
-      let parentIdx = Math.floor( (idx - 1) / 2);
-      let parent = this.values[parentIdx];
-      if (element.priority <= parent.priority) { break; }
-      this.values[parentIdx] = element;
-      this.values[idx] = parent;
-      idx = parentIdx;
-    }
-  }
-
-  dequeue() {
-    var max = this.values[0];
-    var end = this.values.pop();
-
-    if (this.values.length > 0) {
-      this.values[0] = end;
-      this.sinkDown();
-    }
-    return max;
-  }
-
-  sinkDown() {
-    let idx = 0;
-    var len = this.values.length;
-    var element = this.values[0];
-
-    while(true) {
-      let leftChildIdx = 2 * idx + 1;
-      let rightChildIdx = 2 * idx + 2;
-      let leftChild;
-      let rightChild;
-      let swap = null;
-
-      if (leftChildIdx < length) {
-        leftChild = this.values[leftChildIdx];
-        if (leftChild.priority > element.priority) {
-          swap = leftChildIdx;
-        }
-      }
-
-      if (rightChildIdx < length) {
-        rightChild = this.values[rightChildIdx];
-        if ( (swap === null && rightChild.priority > element.priority) || (swap !== null && rightChild.priority > leftChild.priority)) {
-          swap = rightChildIdx;
-        }
-      }
-
-      if (swap === null) { break; }
-      this.values[idx] = this.values[swap];
-      this.values[swap] = element;
-      idx = swap;
-    }
   }
 }
