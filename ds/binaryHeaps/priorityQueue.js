8e5d2695b245114078b90fe3b207393711afae54
Brian Kim <briaansjkim@gmail.com>
2020-02-27 11:50:15 -0500

    added set up comments

--- ds/binaryHeaps/priorityQueue.js
+++ ds/binaryHeaps/priorityQueue.js
@@ -1,10 +1,23 @@
 function Node(val, priority) {
+  // val doesn't quite matter because the priority queue will be constructed using priority
   this.val = val;
   this.priority = priority;
 }
 
+// going to be based on a minBinaryHeap so the lower the number for priority, the higher the priority
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
+
+  // enqueue
+  // accepts a value and a priority
+  // makes a new node
+  // places it in the right spot based on priority
+
+
+  // dequeue
+  // removes root element
+  // returns it
+  // rearranges the heap using priority
 }
