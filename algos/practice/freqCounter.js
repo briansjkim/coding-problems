ae9c3f88a76c8588025a4f2fd7c907de101a82e0
Brian Kim <briaansjkim@gmail.com>
2020-04-28 13:21:56 -0400

    practice

---
+++ algos/practice/freqCounter.js
@@ -0,0 +1,2 @@
+// This pattern uses objects or sets to collect values/frequencies of values
+// This can often avoid the need for nested loops or Exponential time complexity operations w/ arrays/strings
