23db66618c894635f560b14b5f4fdb5da69d92e7
Brian Kim <briaansjkim@gmail.com>
2020-04-28 15:54:39 -0400

    init problem

--- algos/practice/multPointers.js
+++ algos/practice/multPointers.js
@@ -1,2 +1,7 @@
 // This pattern is creating pointers or values that correspond to an index or position and move towards the beginning, end, or middle based on a certain condition
 // very efficient for solving problems w/ minimal space complexity as well
+
+// Example:
+// write a function called sumZero which accepts a sorted array of integers.
+// The function should find the first pair of where the sum is 0
+// Return an array that includes both values that sum to zero or undefined if a pair does not exist
