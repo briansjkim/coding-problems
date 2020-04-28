39c8db37109e8ec70ec8753aa6d6ac4fb18bd246
Brian Kim <briaansjkim@gmail.com>
2020-04-28 13:23:58 -0400

    practice

--- algos/practice/freqCounter.js
+++ algos/practice/freqCounter.js
@@ -1,2 +1,13 @@
 // This pattern uses objects or sets to collect values/frequencies of values
 // This can often avoid the need for nested loops or Exponential time complexity operations w/ arrays/strings
+// This pattern will be linear
+
+// Example:
+// Write a function called same, which accepts 2 arrays.
+// Function should return true if every value in the array has its corresponding value squared in the second array.
+// The frequency of values must be the same
+
+// same([1, 2, 3], [4, 1, 9]) // true
+// same([1, 2, 3], [1, 9]) // false
+// same([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
+
