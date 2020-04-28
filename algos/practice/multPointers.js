d119e9d61988eae8f239123acebad5dbc2facd7f
Brian Kim <briaansjkim@gmail.com>
2020-04-28 15:56:14 -0400

    added ioce

--- algos/practice/multPointers.js
+++ algos/practice/multPointers.js
@@ -10,3 +10,8 @@
 // sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
 // sumZero([-2, 0, 1, 3]) // undefined
 // sumZero([1, 2, 3]) // undefined
+
+// i: sorted array of integers
+// o: array of 2 integers or undefined
+// c: linear time complexity. constant space complexity?
+// e:
