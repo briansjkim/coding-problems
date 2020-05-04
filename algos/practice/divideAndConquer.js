32768a545710b00e8e5bfda67e8017e240803dd0
Brian Kim <briaansjkim@gmail.com>
2020-05-04 11:51:34 -0400

    added sorting examples

--- algos/practice/divideAndConquer.js
+++ algos/practice/divideAndConquer.js
@@ -1,2 +1,4 @@
 // This pattern involes dividing a data set into smaller chunks and then repeating a process w/ a subset of data
 // This pattern can tremendously decrease time complexity
+
+// Ex: Quick Sort and Merge Sort
