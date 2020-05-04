b2fc08ee3c5164fac794a110849cfa2aa0357fd2
Brian Kim <briaansjkim@gmail.com>
2020-05-04 11:52:52 -0400

    added problem

--- algos/practice/divideAndConquer.js
+++ algos/practice/divideAndConquer.js
@@ -2,3 +2,8 @@
 // This pattern can tremendously decrease time complexity
 
 // Ex: Quick Sort and Merge Sort
+
+// Example:
+// Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located.
+// If the value is not found, return -1
+
