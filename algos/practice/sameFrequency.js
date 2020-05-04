93ed4f745502c9d4986a5a22c7b2e8d50df16c00
Brian Kim <briaansjkim@gmail.com>
2020-05-04 16:21:10 -0400

    implemented sameFrequency in linear time complexity

--- algos/practice/sameFrequency.js
+++ algos/practice/sameFrequency.js
@@ -15,6 +15,41 @@
  * e:
  */
 
-var sameFrequency = function (int1, int2) {
+var sameFrequency = function (num1, num2) {
+  let int1 = num1.toString();
+  let int2 = num2.toString();
+  let freq1 = {};
+  let freq2 = {};
 
+  for (let i = 0; i < int1.length; i++) {
+    if (freq1[int1[i]]) {
+      freq1[int1[i]]++;
+    } else {
+      freq1[int1[i]] = 1;
+    }
+  }
+
+  for (let j = 0; j < int2.length; j++) {
+    if (freq2[int2[j]]) {
+      freq2[int2[j]]++;
+    } else {
+      freq2[int2[j]] = 1;
+    }
+  }
+
+  for (let key in freq1) {
+    if (!(key in freq2)) {
+      return false;
+    }
+    if (freq1[key] !== freq2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+// console.log(sameFrequency(182, 281)) // true
+// console.log(sameFrequency(34, 14)) // false
+// console.log(sameFrequency(3589578, 5879385)) // true
+// console.log(sameFrequency(22, 222)) // false
