0e5c156c1728291f8fb56039fb3fa3bd1b6a92af
Brian Kim <briaansjkim@gmail.com>
2020-05-05 11:08:13 -0400

    added ioce

--- algos/practice/areThereDuplicates.js
+++ algos/practice/areThereDuplicates.js
@@ -1,8 +1,13 @@
 /**
- * Implement a function called, areThereDuplicates which accepts a variabel number of arguments, and checks whether there are any duplicates among the arguments passed in.
+ * Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.
  *
  * Examples:
  * areThereDuplicates(1, 2, 3) // false
  * areThereDuplicates(1, 2, 2) // true
  * areThereDuplicates('a', 'b', 'c', 'a') // true
+ *
+ * i: variable number of integers
+ * o: boolean
+ * c: linear time complexity, linear space complexity
+ * e:
  */
