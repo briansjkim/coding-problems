ec783fc5502918b239996b2e7c55442b6a5c62ce
Brian Kim <briaansjkim@gmail.com>
2020-05-05 13:56:34 -0400

    init problem

---
+++ algos/practice/maxSubArraySum.js
@@ -0,0 +1,4 @@
+/**
+ * Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+ * Note that a subarray must consist of CONSECUTIVE elements from the original array.
+ */
