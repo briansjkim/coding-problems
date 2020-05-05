21c42744e56c5916fe7c6587f4ec7856bd82c5af
Brian Kim <briaansjkim@gmail.com>
2020-05-05 13:58:16 -0400

    added testcases

--- algos/practice/maxSubArraySum.js
+++ algos/practice/maxSubArraySum.js
@@ -1,4 +1,11 @@
 /**
  * Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
  * Note that a subarray must consist of CONSECUTIVE elements from the original array.
+ *
+ * Examples:
+ * maxSubarraySum([100, 200, 300, 400], 2) // 700
+ * maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
+ * maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) //5
+ * maxSubarraySum([-3, -2, 7, -4, 1, -1, 4, -2, 1], 2) // 5
+ * maxSubarraySum([2, 3], 3) // null
  */
