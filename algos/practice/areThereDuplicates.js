29b9b41ce048d35b142651dac5e0af1a0b6dfe79
Brian Kim <briaansjkim@gmail.com>
2020-05-05 11:05:51 -0400

    init problem with examples/test cases

---
+++ algos/practice/areThereDuplicates.js
@@ -0,0 +1,8 @@
+/**
+ * Implement a function called, areThereDuplicates which accepts a variabel number of arguments, and checks whether there are any duplicates among the arguments passed in.
+ *
+ * Examples:
+ * areThereDuplicates(1, 2, 3) // false
+ * areThereDuplicates(1, 2, 2) // true
+ * areThereDuplicates('a', 'b', 'c', 'a') // true
+ */
