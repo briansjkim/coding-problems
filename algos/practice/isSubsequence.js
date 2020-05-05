a5c24ee72184c8e8d6654c31cde9f9912039eef8
Brian Kim <briaansjkim@gmail.com>
2020-05-05 11:59:01 -0400

    implemented isSubsequence

--- algos/practice/isSubsequence.js
+++ algos/practice/isSubsequence.js
@@ -8,3 +8,30 @@
  * isSubsequence('abc', 'abracadabra') // true
  * isSubsequence('abc', 'acb') // false (order matters)
  */
+
+var isSubsequence = function (str1, str2) {
+  if (str1.length > str2.length || !str1) { return false; }
+
+  let i = 0;
+  let j = 0;
+
+  while (i < str1.length && j < str2.length) {
+    if (str1[i] === str2[j]) {
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  if (i === str1.length) {
+    return true;
+  } else if (j === str2.length) {
+    return false;
+  }
+}
+
+console.log(isSubsequence('hello', 'hello world')) // true
+console.log(isSubsequence('sing', 'sting')) // true
+console.log(isSubsequence('abc', 'abracadabra')) // true
+console.log(isSubsequence('abc', 'acb')) // false (order matters)
