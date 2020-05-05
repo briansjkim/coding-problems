710c674b1796a2b6001604f5719d742f8671905b
Brian Kim <briaansjkim@gmail.com>
2020-05-05 12:00:34 -0400

    edited first solution and added another solution

--- algos/practice/isSubsequence.js
+++ algos/practice/isSubsequence.js
@@ -18,10 +18,8 @@ var isSubsequence = function (str1, str2) {
   while (i < str1.length && j < str2.length) {
     if (str1[i] === str2[j]) {
       i++;
-      j++;
-    } else {
-      j++;
     }
+    j++;
   }
 
   if (i === str1.length) {
@@ -29,6 +27,24 @@ var isSubsequence = function (str1, str2) {
   } else if (j === str2.length) {
     return false;
   }
+}
+
+var isSubsequence = function (str1, str2) {
+  if (str1.length > str2.length || !str1) { return false; }
+
+  let i = 0;
+  let j = 0;
+
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+    if (i === str1.length) {
+      return true;
+    }
+    j++;
+  }
+  return false
 }
 
 console.log(isSubsequence('hello', 'hello world')) // true
