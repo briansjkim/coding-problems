082d211433a85da9f2290bea1b020fd70be81457
Brian Kim <briaansjkim@gmail.com>
2020-05-05 11:28:19 -0400

    implement another solution

--- algos/practice/areThereDuplicates.js
+++ algos/practice/areThereDuplicates.js
@@ -13,21 +13,40 @@
  */
 
 // linear time complexity, linear space complexity
-var areThereDuplicates = function () {
-  let storage = {};
+// var areThereDuplicates = function () {
+//   let storage = {};
 
-  for (let i = 0; i < arguments.length; i++) {
-    if (storage[arguments[i]]) {
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (storage[arguments[i]]) {
+//       return true;
+//     } else {
+//       storage[arguments[i]] = true;
+//     }
+//   }
+
+//   return false;
+// }
+
+var areThereDuplicates = function (...args) {
+  args.sort((a, b) => a > b);
+
+  let i = 0;
+  let j = 1;
+
+  while (j < args.length) {
+    if (args[i] === args[j]) {
       return true;
     } else {
-      storage[arguments[i]] = true;
+      i++;
+      j++;
     }
   }
-
   return false;
 }
 
 console.log(areThereDuplicates(1, 2, 3));
 console.log(areThereDuplicates(1, 2, 2));
 console.log(areThereDuplicates('a', 'b', 'c', 'a'));
+console.log(areThereDuplicates('a', 'b', 'b', 'c', 'a', 'd'));
+console.log(areThereDuplicates(1, 5, 3, 2, 7));
 
