8bf263e5f422cca49f754c847184b364cd285934
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:04:24 -0400

    implemented factorial

--- algos/recursion/factorial.js
+++ algos/recursion/factorial.js
@@ -12,6 +12,15 @@
  * factorial(7) // 5040
  */
 
-var factorial = function () {
-
+var factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
+
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
