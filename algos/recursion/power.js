b73e2c16dc9eeb6aba86ce1b9aab76d5b2a183e1
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:00:00 -0400

    implemented power

--- algos/recursion/power.js
+++ algos/recursion/power.js
@@ -8,6 +8,14 @@
  * power(2, 4) // 16
  */
 
-var power = function () {
-
+var power = function (base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
 }
+
+console.log(power(2, 0)) // 1
+console.log(power(2, 2)) // 4
+console.log(power(2, 4)) // 16
