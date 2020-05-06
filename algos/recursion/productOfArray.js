0bfdb928ef8148276050bba4f79e2b0e967b7df1
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:14:42 -0400

    added testcases/examples

--- algos/recursion/productOfArray.js
+++ algos/recursion/productOfArray.js
@@ -1,3 +1,7 @@
 /**
  * Write a function called productOfArray which takes in an array of number and returns the product of them all
+ *
+ * Examples:
+ * productOfArray([1, 2, 3]) // 6
+ * productOfArray([1, 2, 3, 10]) // 60
  */
