4770da03135435452e3496bb6dc13bba660d595e
Brian Kim <briaansjkim@gmail.com>
2020-05-06 14:54:56 -0400

    implemented someRecursive

--- algos/recursion/someRecursive.js
+++ algos/recursion/someRecursive.js
@@ -13,6 +13,17 @@ const isOdd = val => val % 2 !== 0;
 
 
 var someRecursive = function (ints, cb) {
-
+  if (ints.length === 0) {
+    return false;
+  } else if (cb(ints[0])) {
+    return true;
+  } else {
+    return someRecursive(ints.slice(1), cb);
+  }
 }
+
+console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
+console.log(someRecursive([4, 6, 8], val => val > 10)); // false
 
