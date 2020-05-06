43da167a0324debb702a0dd3580201deb82fa4c0
Brian Kim <briaansjkim@gmail.com>
2020-05-06 13:37:25 -0400

    init problem

---
+++ algos/recursion/reverse.js
@@ -0,0 +1,7 @@
+/**
+ * Write a recursive function called reverse which accepts a string and returns a new string in reverse
+ *
+ * Examples:
+ * reverse('awesome') // 'emosewa'
+ * reverse('rithmschool') // 'loohcsmhtir'
+ */
