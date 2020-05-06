6378e9ad05881841ba5d21659d8f24d17b624830
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:18:17 -0400

    init problem

---
+++ algos/recursion/recursiveRange.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+ *
+ * Examples
+ * recursiveRange(6) // 21
+ * recursiveRange(10) // 55
+ */
