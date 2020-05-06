7f2e8b306d2efb3b1463e53ffb3115b21fabe14e
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:23:10 -0400

    implemented recursiveRange

--- algos/recursion/recursiveRange.js
+++ algos/recursion/recursiveRange.js
@@ -12,6 +12,14 @@
  */
 
 var recursiveRange = function (int) {
-
+  if (int === 0) {
+    return 0;
+  } else {
+    return int += recursiveRange(int - 1);
+  }
 }
 
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
+console.log(recursiveRange(3));//6
+console.log(recursiveRange(0)); // 0
