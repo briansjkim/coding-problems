911e45e64b879036523c6a96b26776fbc17bdb26
Brian Kim <briaansjkim@gmail.com>
2020-05-06 12:01:53 -0400

    added testcases/examples

--- algos/recursion/factorial.js
+++ algos/recursion/factorial.js
@@ -4,4 +4,10 @@
  *
  * factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24
  * factorial zero (0!) is always 1
+ *
+ * Examples:
+ * factorial(1) // 1
+ * factorial(2) // 2
+ * factorial(4) // 24
+ * factorial(7) // 5040
  */
