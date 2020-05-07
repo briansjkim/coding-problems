06adcee2219ebeb07cde0bade26caaf53d93ea70
Brian Kim <briaansjkim@gmail.com>
2020-05-07 14:40:44 -0400

    added ioce

--- algos/insertionSort/practice.js
+++ algos/insertionSort/practice.js
@@ -1,3 +1,8 @@
 /**
  * Builds up the sort by gradually creating a larger left portion which is always sorted
+ *
+ * i: array of unsorted integers
+ * o: array of sorted integers
+ * c: quadratic
+ * e:
  */
