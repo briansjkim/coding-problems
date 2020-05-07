b14c9b836e0d86b54a41f47f2b52d3dc2f7cdf78
Brian Kim <briaansjkim@gmail.com>
2020-05-07 11:28:21 -0400

    init problem

--- algos/practice/binarySearch.js
+++ algos/practice/binarySearch.js
@@ -4,3 +4,23 @@
 //o: index of the value or -1 if the value isn't there
 //c: log n time complexity
 //e:
+
+var binarySearch = function (ints, n) {
+  let mid = Math.floor(ints.length / 2);
+
+  if (n === ints[mid]) {
+    console.log(mid)
+    return mid;
+  } else if (n > ints[mid]) {
+    console.log(mid, ints[mid])
+    return binarySearch(ints.slice(mid + 1), n);
+  } else if (n < ints[mid]) {
+    return binarySearch(ints.slice(0, mid - 1), n);
+  } else {
+    return -1;
+  }
+}
+
+// console.log(binarySearch([1, 2, 3, 4, 5, 6], 4)) // 3
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)) // 5
+// console.log(binarySearch([1, 2, 3, 4, 5, 6], 11)) // -1
