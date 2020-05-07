3ce9373d4eb071bb80bec6acef2d25fe6e3e77e9
Brian Kim <briaansjkim@gmail.com>
2020-05-07 11:33:41 -0400

    implemented binarySearch

--- algos/recursion/binarySearch.js
+++ algos/recursion/binarySearch.js
@@ -2,7 +2,26 @@
 
 /**
  * i: array of sorted integers and a value
- * o: index of value or -1 if the value isn't in the array
+ * o: boolean -- true if value is there, false if the value isn't
  * c: log n time complexity
  * e:
  */
+
+var binarySearch = function (ints, n) {
+  let mid = Math.floor(ints.length / 2);
+
+  if (n === ints[mid]) {
+    return true;
+  } else if (n > ints[mid]) {
+    return binarySearch(ints.slice(mid + 1), n);
+  } else if (n < ints[mid]) {
+    return binarySearch(ints.slice(0, mid - 1), n);
+  } else {
+    return false;
+  }
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4)) // true
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)) // true
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 11)) // false
+
