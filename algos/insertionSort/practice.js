f0954ce6565011bfec2d12c4d28e35953a5b94e9
Brian Kim <briaansjkim@gmail.com>
2020-05-07 14:52:18 -0400

    implemented insertionSort

--- algos/insertionSort/practice.js
+++ algos/insertionSort/practice.js
@@ -7,6 +7,23 @@
  * e:
  */
 
-var insertionSort = function (ints) {
-
+var swap = function (arr, idx1, idx2) {
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
 }
+
+
+var insertionSort = function (ints) {
+  for (let i = ints.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (ints[i] < ints[j]) {
+        swap(ints, i, j);
+      }
+    }
+  }
+  return ints;
+}
+
+console.log(insertionSort([2, 1, 9, 74, 6]));
+console.log(insertionSort([5, 3, 4, 1, 2]));
