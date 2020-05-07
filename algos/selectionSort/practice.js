03ab4db07a67a788766f45ee906508a872b9cc22
Brian Kim <briaansjkim@gmail.com>
2020-05-07 14:27:30 -0400

    init problem

---
+++ algos/selectionSort/practice.js
@@ -0,0 +1,8 @@
+/**
+ * Similar to bubble sort, but instead of first placing large values into sorted position, it places small values into sorted position
+ *
+ * i: array of unsorted integers
+ * o: array of sorted integers
+ * c:
+ * e:
+ */
