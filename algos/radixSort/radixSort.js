dab5cdd9188e1770a5a8f9a639980d6ab006a047
Brian Kim <briaansjkim@gmail.com>
2020-05-08 14:53:49 -0400

    implemented getDigit helper function

--- algos/radixSort/radixSort.js
+++ algos/radixSort/radixSort.js
@@ -7,4 +7,17 @@
  *
  * first looks at the right-most digit in each number
  * then second to right-most, so on
+ *
+ * radix sort helpers
+ * getDigit(num, place) = returns the digit in num at the given place value
  */
+
+var getDigit = (num, place) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+console.log(getDigit(7323, 2)); // 3
+console.log(getDigit(7323, 1)); // 2
+console.log(getDigit(7323, 0)); // 3
+console.log(getDigit(7323, 4)); // 0
+console.log(getDigit(7323, 3)); // 7
