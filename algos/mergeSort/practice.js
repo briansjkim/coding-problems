c54a3bd0924802206780cb265abc3d6cdc8e2abb
Brian Kim <briaansjkim@gmail.com>
2020-05-08 11:27:09 -0400

    implemented mergeSort

--- algos/mergeSort/practice.js
+++ algos/mergeSort/practice.js
@@ -11,9 +11,45 @@
 
 // helper function used to compare values and merge arrays
 var merge = function (ints1, ints2) {
+  let newInts = [];
+  let i = 0;
+  let j = 0;
 
+  while (i < ints1.length && j < ints2.length) {
+    if (ints1[i] < ints2[j]) {
+      newInts.push(ints1[i]);
+      i++;
+    } else {
+      newInts.push(ints2[j]);
+      j++;
+    }
+  }
+
+  while (i < ints1.length) {
+    newInts.push(ints1[i]);
+    i++;
+  }
+
+  while (j < ints2.length) {
+    newInts.push(ints2[j]);
+    j++;
+  }
+
+  return newInts;
 }
 
 var mergeSort = function (ints) {
+  // arrays of 0 or 1 are always sorted
+  if (ints.length <= 1) { return ints; }
 
+  let mid = Math.floor(ints.length / 2);
+  // breaks down the left side of the array until there's 0 or 1 elements
+  let left = mergeSort(ints.slice(0, mid));
+  // same with the right side
+  let right = mergeSort(ints.slice(mid));
+
+  // merge the two together
+  return merge(left, right);
 }
+
+console.log(mergeSort([4, 3, 2, 7, 10, 99, 50, 100]))
