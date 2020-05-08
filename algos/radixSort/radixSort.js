d452939d36b920506c62474cac513d00d43ba0f2
Brian Kim <briaansjkim@gmail.com>
2020-05-08 14:48:51 -0400

    added high level approach

--- algos/radixSort/radixSort.js
+++ algos/radixSort/radixSort.js
@@ -3,4 +3,8 @@
  * Never makes comparisons between elements
  * Instead, it exploits the fact that info about the size of a number is envoded in the number of digits
  * More digits means a bigger number
+ *
+ *
+ * first looks at the right-most digit in each number
+ * then second to right-most, so on
  */
