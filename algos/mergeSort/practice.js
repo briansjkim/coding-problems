72e67415348beb86dddc0e84828132f3b177a30d
Brian Kim <briaansjkim@gmail.com>
2020-05-08 11:43:13 -0400

    more practice

--- algos/mergeSort/practice.js
+++ algos/mergeSort/practice.js
@@ -9,47 +9,48 @@
  * e:
  */
 
-// helper function used to compare values and merge arrays
+// helper function
 var merge = function (ints1, ints2) {
-  let newInts = [];
+  let result = [];
   let i = 0;
   let j = 0;
 
   while (i < ints1.length && j < ints2.length) {
     if (ints1[i] < ints2[j]) {
-      newInts.push(ints1[i]);
+      result.push(ints1[i]);
       i++;
     } else {
-      newInts.push(ints2[j]);
+      result.push(ints2[j]);
       j++;
     }
   }
 
   while (i < ints1.length) {
-    newInts.push(ints1[i]);
+    result.push(ints1[i]);
     i++;
   }
 
   while (j < ints2.length) {
-    newInts.push(ints2[j]);
+    result.push(ints2[j]);
     j++;
   }
 
-  return newInts;
+  return result;
 }
 
 var mergeSort = function (ints) {
-  // arrays of 0 or 1 are always sorted
+  // if an array is 0 or 1 elements, it's sorted
   if (ints.length <= 1) { return ints; }
 
   let mid = Math.floor(ints.length / 2);
-  // breaks down the left side of the array until there's 0 or 1 elements
+
+  // break down left side of the array until it's 0 or 1 elements
   let left = mergeSort(ints.slice(0, mid));
-  // same with the right side
+  // same with right side
   let right = mergeSort(ints.slice(mid));
 
-  // merge the two together
   return merge(left, right);
 }
 
-console.log(mergeSort([4, 3, 2, 7, 10, 99, 50, 100]))
+console.log(mergeSort([4, 3, 2, 7, 10, 99, 50, 100]));
+console.log(mergeSort([1, 5, 10, 4, 7, 11]));
