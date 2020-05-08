00b342f4d90f78e312472d87a37395810d54dc57
Brian Kim <briaansjkim@gmail.com>
2020-05-08 11:14:38 -0400

    init problem

---
+++ algos/mergeSort/practice.js
@@ -0,0 +1,5 @@
+/**
+ * Combination of two things - merging and sorting
+ * Exploits the fact that arrays of 0 or 1 element are always sorted
+ * Works by decomposing an array into SMALLER arrays of 0 or 1 element, then building up a newly sorted array
+ */
