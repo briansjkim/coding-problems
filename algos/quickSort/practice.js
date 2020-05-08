720874e849c3f0af3371b92157759eb5c25d07cf
Brian Kim <briaansjkim@gmail.com>
2020-05-08 13:54:03 -0400

    created swap function

--- algos/quickSort/practice.js
+++ algos/quickSort/practice.js
@@ -6,6 +6,21 @@
  *  ideally, the pivot should be chosen so that it's roughly the median value
  */
 
-function pivot(arr, start = 0, end = arr.length + 1) {
+function swap(arr, idx1, idx2) {
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+}
 
+
+function pivot(arr, start = 0, end = arr.length + 1) {
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, i, swapIdx);
+    }
+  }
 }
