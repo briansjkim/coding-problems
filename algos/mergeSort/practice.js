c77905e88f2273a1cfd53ecfbada01bbb554419e
Brian Kim <briaansjkim@gmail.com>
2020-05-08 11:19:50 -0400

    added ioce

--- algos/mergeSort/practice.js
+++ algos/mergeSort/practice.js
@@ -2,4 +2,9 @@
  * Combination of two things - merging and sorting
  * Exploits the fact that arrays of 0 or 1 element are always sorted
  * Works by decomposing an array into SMALLER arrays of 0 or 1 element, then building up a newly sorted array
+ *
+ * i: unsorted array of integers
+ * o: sorted array of integers
+ * c: O(n + m) time complexity and O(n + m) space complexity
+ * e:
  */
