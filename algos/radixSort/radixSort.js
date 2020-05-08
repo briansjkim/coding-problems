51cddd57dc78e6565bd29d31aabae339aa5de620
Brian Kim <briaansjkim@gmail.com>
2020-05-08 14:28:07 -0400

    init algo

---
+++ algos/radixSort/radixSort.js
@@ -0,0 +1,6 @@
+/**
+ * Special sorting algo that works on lists of numbers
+ * Never makes comparisons between elements
+ * Instead, it exploits the fact that info about the size of a number is envoded in the number of digits
+ * More digits means a bigger number
+ */
