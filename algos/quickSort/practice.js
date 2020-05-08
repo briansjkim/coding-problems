a6b6c9f5872483bdef42e301c8f1eb48887eb51c
Brian Kim <briaansjkim@gmail.com>
2020-05-08 14:08:37 -0400

    implemented quickSort

--- algos/quickSort/practice.js
+++ algos/quickSort/practice.js
@@ -33,4 +33,11 @@ function pivot(arr, start = 0, end = arr.length + 1) {
   return swapIdx;
 }
 
-
+var quickSort = function (arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIdx = pivot(arr, left, right);
+    quickSort(arr, left, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, right);
+  }
+  return arr;
+}
