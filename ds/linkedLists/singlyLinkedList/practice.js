a6174fd38e93c5a6372342b6921557c61c324cac
Brian Kim <briaansjkim@gmail.com>
2020-05-11 12:18:30 -0400

    init singly linkedlist

--- ds/linkedLists/singlyLinkedList/practice.js
+++ ds/linkedLists/singlyLinkedList/practice.js
@@ -10,3 +10,11 @@ function Node(val) {
   this.val = val;
   this.next = null;
 }
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
