29ce05d510c663322dd0f3b966355e40d14871a3
Brian Kim <briaansjkim@gmail.com>
2020-05-11 11:48:17 -0400

    init singly linked list

---
+++ ds/linkedLists/singlyLinkedList/practice.js
@@ -0,0 +1,7 @@
+/**
+ * A linked list contains a head, tail, and length (sometimes) property
+ *
+ * Linked Lists consists of nodes and each node has a value and a pointer to another node or null
+ *
+ * Singly Linked Lists are only connected to the next node, not the previous node
+ */
