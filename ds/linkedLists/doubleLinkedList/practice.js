3c3dcfa89d349c604ab5e020cafc1ecb27dcee8b
Brian Kim <briaansjkim@gmail.com>
2020-05-12 13:49:38 -0400

    init doubly ll

--- ds/linkedLists/doubleLinkedList/practice.js
+++ ds/linkedLists/doubleLinkedList/practice.js
@@ -7,3 +7,11 @@ function Node(val) {
   this.next = null;
   this.prev = null;
 }
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
