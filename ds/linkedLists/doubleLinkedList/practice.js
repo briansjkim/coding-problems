f74672dd955f7ee6580bde92943807a209f00e82
Brian Kim <briaansjkim@gmail.com>
2020-05-12 13:51:32 -0400

    implemented push function

--- ds/linkedLists/doubleLinkedList/practice.js
+++ ds/linkedLists/doubleLinkedList/practice.js
@@ -14,4 +14,20 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
