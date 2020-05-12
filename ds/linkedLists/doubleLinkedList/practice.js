20c6822595dbac5dff7bbffe7f5248b6cfd3ee68
Brian Kim <briaansjkim@gmail.com>
2020-05-12 13:48:30 -0400

    node function

--- ds/linkedLists/doubleLinkedList/practice.js
+++ ds/linkedLists/doubleLinkedList/practice.js
@@ -1,3 +1,9 @@
 /**
  * Doubly LLs are the same as Singly LLs except that each node has both a next and previous pointer
  */
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+  this.prev = null;
+}
