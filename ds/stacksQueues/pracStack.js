fbf27c5ec550717c64b8b5658fc611192c66a3f3
Brian Kim <briaansjkim@gmail.com>
2020-05-13 13:12:32 -0400

    implemented node function

--- ds/stacksQueues/pracStack.js
+++ ds/stacksQueues/pracStack.js
@@ -3,3 +3,11 @@
  * The last element added to the stack will be the first one to be removed
  * Ex: Stack of plates, call stack, undo/redo, routing
  */
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+
+
