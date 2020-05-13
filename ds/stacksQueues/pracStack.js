c5bfe0edafb44c795a8a9d5d63c43009cb6cef87
Brian Kim <briaansjkim@gmail.com>
2020-05-13 13:19:21 -0400

    implemented stack

--- ds/stacksQueues/pracStack.js
+++ ds/stacksQueues/pracStack.js
@@ -2,6 +2,8 @@
  * Stack: A LIFO data structure
  * The last element added to the stack will be the first one to be removed
  * Ex: Stack of plates, call stack, undo/redo, routing
+ *
+ * Stacks focus on insertion and removal to be constant time. Searching and access (using LLs) are linear
  */
 
 function Node(val) {
