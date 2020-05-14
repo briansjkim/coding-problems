d0b42a6e318185767e314ca8d3a0999fff0d485d
Brian Kim <briaansjkim@gmail.com>
2020-05-14 12:55:49 -0400

    implemented insert iteratively

--- ds/BST/practice.js
+++ ds/BST/practice.js
@@ -26,4 +26,35 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    var current = this.root;
+    while (true) {
+      if (newNode.val === current.val) {
+        return undefined;
+      }
+      if (newNode.val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else if (newNode.val > current.val) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+    return this;
+  }
 }
