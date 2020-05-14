5531968ca2bbaef6c29241106b1061243f76d942
Brian Kim <briaansjkim@gmail.com>
2020-05-14 12:25:24 -0400

    init bst

---
+++ ds/BST/practice.js
@@ -0,0 +1,11 @@
+/**
+ * A tree is a data structure that consists of nodes in a parent/child relationship
+ *
+ * Nonlinear data structure which means that there are many different paths to take
+ *
+ * Root - the top node in a tree
+ * child - a node directly connected to another node when moving away from the root
+ * parent- the converse notion of a child
+ * siblings - a group of nodes with the same parent
+ * leaf - a node w/ no children
+ */
