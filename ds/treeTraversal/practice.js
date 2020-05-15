609cc6a1be3e9e0987934437fea6574031b1ac46
Brian Kim <briaansjkim@gmail.com>
2020-05-15 11:21:57 -0400

    added insert function

--- ds/treeTraversal/practice.js
+++ ds/treeTraversal/practice.js
@@ -4,4 +4,38 @@ function Node(val) {
   this.right = null;
 }
 
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
+  insert(val) {
+    var newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (newNode.val === current.val) {
+          return undefined;
+        }
+        if (newNode.val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (newNode.val > current.val) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+    return this;
+  }
+}
